1218cf8824a00aa95d2610a075bf0342673a6a97
Saksham Sharma <122073643+sakshamsharma99@users.noreply.github.com>
2023-06-12 19:56:13 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,56 @@
+print(
+    '''
+ _____________________
+|  _________________  |
+| | CS           0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+
+'''
+)
+continue_loop = True
+while continue_loop != False:    
+    n1 = int(input("What's the first number?: "))
+    print('''
+    +
+    -
+    *
+    /
+    ''')
+
+    def inner(n1):
+                   
+        operator = input("Pick an operation: ")
+
+        n2 = int(input("What's the next number?: "))
+        solution = 0
+        if operator == '-':
+            solution = n1 - n2
+        elif operator == '+':
+            solution = n1 + n2
+        elif operator == '*':
+            solution = n1 * n2
+        elif operator == '/':
+            solution = n1 / n2
+        else:
+            print("Wrong input")
+
+        print(f"{n1} {operator} {n2} = {solution}")
+
+        game_over = input(f"Type 'y' to continue calculating with {solution}, or type 'n' to stat a new calculation: ")
+
+        if game_over == 'n':
+            solution = 0
+        elif game_over == 'y':
+            n1 = solution
+            inner(n1)
+    inner(n1)
